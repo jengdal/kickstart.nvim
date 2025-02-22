f2077f59c7abc3cf5dfe31e64a55dbf1a7967bd8
John Engdal <john@walkbase.com>
2025-02-22 21:48:22 +0200

    python auto import and import sort working

--- init.lua
+++ init.lua
@@ -664,12 +664,15 @@ require('lazy').setup({
           settings = {
             pyright = {
               -- Using Ruff's import organizer
-              --disableOrganizeImports = true,
+              disableOrganizeImports = true,
+              openFilesOnly = false,
             },
             python = {
               analysis = {
                 -- Ignore all files for analysis to exclusively use Ruff for linting
                 ignore = { '*' },
+                autoImportCompletions = true,
+                autoSearchPaths = true,
               },
             },
           },
@@ -684,7 +687,19 @@ require('lazy').setup({
         ts_ls = {},
         --
 
-        ruff = {}, -- linter & formatter (includes flake8, pep8, black, isort, etc.)
+        ruff = {
+          on_attach = function(client, bufnr)
+            -- Disable hover in favor of Pyright
+            client.server_capabilities.hoverProvider = false
+            -- Organize imports via code action on save
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              callback = function()
+                vim.lsp.buf.code_action { context = { only = { 'source.organizeImports' } }, apply = true }
+              end,
+              buffer = bufnr,
+            })
+          end,
+        }, -- linter & formatter (includes flake8, pep8, black, isort, etc.)
         debugpy = {}, -- debugger
         taplo = {}, -- LSP for toml (e.g., for pyproject.toml files)
         sqlfluff = {},
