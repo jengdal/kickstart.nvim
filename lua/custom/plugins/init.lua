a67f9e866091bdbdea1855e6d54a77968cfcdd63
John Engdal <john@walkbase.com>
2025-02-26 17:14:03 +0200

    Move custom plugins to own files

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -44,157 +44,30 @@ return {
   --    --   config = bar
   --    --   end,
   --  },
+
+  -- Problems with other plugins, like dadbod-ui:
   --  {
-  --    'Mofiqul/vscode.nvim',
-  --    config = function()
-  --      --     vim.o.background = 'light'
+  --    'rmagatti/auto-session',
+  --    lazy = false,
   --
-  --      --   local c = require('vscode.colors').get_colors()
-  --      require('vscode').setup {
-  --        -- Alternatively set style in setup
-  --        style = 'light',
-  --        --
-  --        --        -- Enable transparent background
-  --        --        transparent = true,
-  --        --
-  --        --        -- Enable italic comment
-  --        --        italic_comments = true,
-  --        --
-  --        --        -- Underline `@markup.link.*` variants
-  --        --        underline_links = true,
-  --        --
-  --        --        -- Disable nvim-tree background color
-  --        --        disable_nvimtree_bg = true,
-  --        --
-  --        --        -- Override colors (see ./lua/vscode/colors.lua)
-  --        --        color_overrides = {
-  --        --          vscLineNumber = '#FFFFFF',
-  --        --        },
-  --        --
-  --        --        -- Override highlight groups (see ./lua/vscode/theme.lua)
-  --        --        group_overrides = {
-  --        --          -- this supports the same val table as vim.api.nvim_set_hl
-  --        --          -- use colors from this colorscheme by requiring vscode.colors!
-  --        --          Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
-  --        --        },
-  --      }
-  --      -- require('vscode').load()
-  --
-  --      -- load the theme without affecting devicon colors.
-  --      -- vim.cmd.colorscheme 'vscode'
-  --    end,
+  --    ---enables autocomplete for opts
+  --    ---@module "auto-session"
+  --    ---@type AutoSession.Config
+  --    opts = {
+  --      suppressed_dirs = { '~/', '~/workspace', '~/Downloads', '/' },
+  --      -- log_level = 'debug',
+  --    },
   --  },
-  {
-    'nickkadutskyi/jb.nvim',
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      --require('jb').setup { transparent = false }
-      vim.cmd.colorscheme 'jb'
-      vim.o.background = 'light'
-    end,
-  },
-  {
-    'nvim-tree/nvim-tree.lua',
-    version = '*',
-    lazy = false,
-    dependencies = {
-      'nvim-tree/nvim-web-devicons',
-    },
-    config = function()
-      require('nvim-tree').setup {
 
-        sort = {
-          sorter = 'case_sensitive',
-        },
-        view = {
-          width = 50,
-        },
-        renderer = {
-          group_empty = true,
-        },
-        filters = {
-          dotfiles = true,
-        },
-        update_focused_file = {
-          enable = true,
-          update_root = false,
-          ignore_list = {},
-        },
-      }
-      local function open_nvim_tree()
-        -- open the tree
-        require('nvim-tree.api').tree.open()
-      end
-      vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
-    end,
-  },
-  {
-    'jiaoshijie/undotree',
-    dependencies = 'nvim-lua/plenary.nvim',
-    config = true,
-    keys = { -- load the plugin only when using it's keybinding:
-      { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>" },
-    },
-  },
-  {
-    'dawsers/telescope-file-history.nvim',
-    config = function()
-      require('file_history').setup {
-        -- This is the location where it will create your file history repository
-        backup_dir = '~/.file-history-git',
-        -- command line to execute git
-        git_cmd = 'git',
-      }
-      require('telescope').load_extension 'file_history'
-    end,
-  },
-  {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    opts = {
-      -- add any options here
-    },
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      'MunifTanjim/nui.nvim',
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      'rcarriga/nvim-notify',
-    },
-    config = function()
-      require('noice').setup {
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-            ['vim.lsp.util.stylize_markdown'] = true,
-            ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
-          },
-        },
-        -- you can enable a preset for easier configuration
-        presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
-        },
-      }
-    end,
-  },
-  {
-    'rmagatti/auto-session',
-    lazy = false,
+  -- {
+  --   'tpope/vim-dadbod',
+  --   'kristijanhusak/vim-dadbod-completion',
+  --   'kristijanhusak/vim-dadbod-ui',
+  -- },
 
-    ---enables autocomplete for opts
-    ---@module "auto-session"
-    ---@type AutoSession.Config
-    opts = {
-      suppressed_dirs = { '~/', '~/workspace', '~/Downloads', '/' },
-      -- log_level = 'debug',
-    },
-  },
+  { 'tpope/vim-dotenv' },
+  -- {
+  --   'knubie/vim-kitty-navigator',
+  --   build = 'cp ./*.py ~/.config/kitty/',
+  -- },
 }
---
+++ lua/custom/plugins/nvim-tree.lua
@@ -0,0 +1,60 @@
+return {
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
+  lazy = false,
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require('nvim-tree').setup {
+
+      sort = {
+        sorter = 'case_sensitive',
+      },
+      view = {
+        width = 60,
+      },
+      renderer = {
+        group_empty = true,
+      },
+      filters = {
+        dotfiles = true,
+        exclude = {
+          '.env',
+          '.direnv',
+          '.prettierrc',
+          '.gitignore',
+          'proxy.conf.*',
+          '.editorconfig',
+        },
+      },
+      update_focused_file = {
+        enable = true,
+        update_root = false,
+        ignore_list = {},
+      },
+      git = {
+        enable = true,
+        --ignore = false,
+        timeout = 500,
+      },
+    }
+
+    -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#workaround-when-using-rmagattiauto-session
+    -- vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+    --   pattern = 'NvimTree*',
+    --   callback = function()
+    --     local api = require 'nvim-tree.api'
+    --     local view = require 'nvim-tree.view'
+    --
+    --     if not view.is_visible() then
+    --       api.tree.open()
+    --     end
+    --   end,
+    -- })
+    local function open_nvim_tree()
+      require('nvim-tree.api').tree.toggle(false, true)
+    end
+    vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
+  end,
+}
